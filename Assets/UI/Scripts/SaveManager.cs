56835e4c7f477c7c9fcfcec404041ca36e99cea8
MMIK0 <35567342+MMIK0@users.noreply.github.com>
2021-11-12 12:55:17 +0200

    Created mainmenu
    Main menu and character selection menu created. Created savemanager which for now saves into playerprefs the wanted character stats and load them back in next scene. Ui script for charactermenus game start

---
+++ Assets/UI/Scripts/SaveManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[DefaultExecutionOrder(-1)]
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager instance;
+    private int chari, str, tec, bp, hp, mon;
+    public ReadyEvent readyEvent;
+
+    public void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+        {
+            Debug.Log("Killing Duplicate Manager");
+            Destroy(gameObject);
+            return;
+        }
+        Debug.Log(instance);
+    }
+
+    public void SaveValues(int charisma, int strength, int tecnowledge, int battlePower, int health, int money)
+    {
+        chari = charisma;
+        str = strength;
+        tec = tecnowledge;
+        bp = battlePower;
+        hp = health;
+        mon = money;
+    }
+
+    public void Checkstats()
+    {
+        Debug.Log(chari + "" + str + "" + tec + "" + bp + "" + hp + "" + mon);
+    }
+
+    public void SaveStats()
+    {
+        PlayerPrefs.SetInt("chari", chari);
+        PlayerPrefs.SetInt("str", str);
+        PlayerPrefs.SetInt("tec", tec);
+        PlayerPrefs.SetInt("bp", bp);
+        PlayerPrefs.SetInt("hp", hp);
+        PlayerPrefs.SetInt("money", mon);
+    }
+}
+[System.Serializable]
+public class ReadyEvent : UnityEvent<bool> { }
---
+++ Assets/UI/Scripts/StartGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class StartGame : MonoBehaviour
+{
+    public Button button;
+
+    public void OnEnable()
+    {
+        SaveManager.instance?.readyEvent.AddListener(SetButtonActive);
+    }
+
+    public void OnDisable()
+    {
+        SaveManager.instance?.readyEvent.RemoveListener(SetButtonActive);
+    }
+
+    public void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    private void SetButtonActive(bool result)
+    {
+        button.interactable = result;
+    }
+
+    public void TestClick()
+    {
+        SaveManager.instance.SaveStats();
+        SceneManager.LoadScene(1);
+    }    
+}
